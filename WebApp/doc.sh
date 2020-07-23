0cdc6cf33970c479e8aee95fe04ef14ae8bd15f0
ralph <ralphhanna@hotmail.com>
2020-07-23 01:35:05 -0400

    remove unwanted files

--- WebApp/doc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rm bpmnServer/docs/api.old -r
-mv bpmnServer/docs/api bpmnServer/docs/api.old
-
-npx typedoc -out bpmnServer/docs/api bpmnServer/src --mode file --readme none --excludePrivate
