5a8850b6950d7a84d330dc208bccd1fe69513f68
ralphhanna <ralphhanna@hotmail.com>
2023-11-02 00:49:24 -0400

    Release 1.5.0

--- WebApp/test/helpers/setup.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-console.log('test/helpers/setup.js');
-
-process.env.NODE_ENV = 'test';
-Error.stackTraceLimit = 20;
-global.expect = require('chai').expect;
-global.assert = require('chai').assert;
--- WebApp/version.js
+++
@@ -1,6 +0,0 @@
-
-Object.defineProperty(exports, "__esModule", { value: true });
-exports._version = void 0;
-const _version = "1.2.1";
-exports._version = _version;
-//# sourceMappingURL=version.js.map
